0f88172169139b00e6b24c8b4873c8a3b0c011dd
MattFMorris <MattFMorris@gmail.com>
2012-03-05 00:29:20 +0000

    stuff

--- GUI/src/TestDungeon.java
+++ GUI/src/TestDungeon.java
@@ -1,6 +1,11 @@
-
+/**
+ * 
+ * @author MattFMorris
+ *
+ */
 public class TestDungeon {
 	int[][] dungeon;
+	PlayerCharacter pc;
 	public TestDungeon() {
 		dungeon = new int[][]{
 						{12,25,25,25,25,13},
@@ -10,8 +15,14 @@ public class TestDungeon {
 						{24,85,85,85,85,26},
 						{15,27,27,27,27,14}
 					};
+		pc = new PlayerCharacter();
 	}
+	
 	public int[][] getDungeon() {
 		return dungeon;
+	}
+	
+	public PlayerCharacter getPC() {
+		return pc;
 	}
 }
