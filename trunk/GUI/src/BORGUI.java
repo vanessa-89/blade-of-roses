fdcb5df88f02f2a3567f203ed8afe4498d17c509
MattFMorris@gmail.com <MattFMorris@gmail.com@6da87519-ff26-698d-a049-a8468f5c8738>
2012-04-27 04:50:49 +0000

    Began new GUI Class

--- trunk/GUI/src/BORGUI.java
+++ trunk/GUI/src/BORGUI.java
@@ -9,4 +9,15 @@ public class BORGUI {
 	MapCanvas map;
 	PlayerCharacter pc;
 	
+	public static void main(String[] args) {
+		BORGUI borgui = new BORGUI();
+		borgui.go();
+	}
+	
+	public void go() {
+		map = new MapCanvas();
+		map.setSize(800, 600);
+		map.setVisible(true);
+	}
+	
 }
