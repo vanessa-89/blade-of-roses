7c8146840f9cb33afc402e5e9b2fea60d315177f
pyro1370@gmail.com <pyro1370@gmail.com@6da87519-ff26-698d-a049-a8468f5c8738>
2012-06-03 19:38:16 +0000

    TestArea created and ready to split a square area into left and right with a door in between in the middle.

---
+++ trunk/BOR/src/com/fetch/bor/bor/TestArea.java
@@ -0,0 +1,53 @@
+package com.fetch.bor.bor;
+
+
+
+public class TestArea {
+	Tile[][] arena;
+	int arenasize;
+	
+	public TestArea(int size){
+		arenasize = size;
+		arena = new Tile[arenasize][arenasize];
+	
+		
+		for (int i=1; i<arenasize-2; i++) {
+			for (int j=1; j<arenasize-2; j++) {
+				arena[i][j].tile[1][1] = 1;
+				arena[i][j].floor = 1;
+			}
+		}
+		
+		for (int i=1; i<arenasize-2; i++) {
+			arena[1][i].tile[0][1] = 1;
+			arena[arenasize-2][i].tile[2][1] = 1;
+			arena[i][1].tile[1][0] = 1;
+			arena[i][arenasize-2].tile[1][2] = 1;
+			arena[i][arenasize/2].tile[1][2] = 1;
+			arena[i][arenasize/2+1].tile[1][0] = 1;		
+		}
+		
+		//top left corners
+		arena[1][1].tile[0][0] = 1;
+		arena[1][arenasize/2+1].tile[0][0] = 1;
+		//top right corners
+		arena[1][arenasize-2].tile[0][2] = 1;
+		arena[1][arenasize/2].tile[0][2] = 1;
+		//bottom left corners
+		arena[arenasize-2][1].tile[2][0] = 1;
+		arena[arenasize-2][arenasize/2+1].tile[2][0] = 1;
+		//bottom right corners
+		arena[arenasize-2][arenasize-2].tile[2][2] = 1;
+		arena[arenasize-2][arenasize/2].tile[2][2] = 1;
+		
+		//doors
+		arena[arenasize/2][arenasize/2].tile[1][0] = 2;
+		arena[arenasize/2][arenasize/2+1].tile[1][0] = 2;
+		
+	}
+	
+	public Tile[][] getTiles(){
+		return arena;
+	}
+}
+
