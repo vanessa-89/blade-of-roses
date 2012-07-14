0002bcdba2e21632827a1d491ce640582515b027
pyro1370@gmail.com <pyro1370@gmail.com@6da87519-ff26-698d-a049-a8468f5c8738>
2012-07-14 23:30:58 +0000

    Finished ProceduralGeneration with ProceduralHallway and ProceduralRoom. ProceduralHallway may need work, essentially just copy pasted from old hallway.java

--- trunk/BOR/src/com/fetch/bor/Procedural/ProceduralRoom.java
+++ trunk/BOR/src/com/fetch/bor/Procedural/ProceduralRoom.java
@@ -1,5 +1,63 @@
 package com.fetch.bor.Procedural;
 
-public class ProceduralRoom extends ProceduralStructure {
+import java.util.Random;
+//import java.util.Stack;
 
+public class ProceduralRoom{
+
+	
+	int width, height;
+	int realX, realY;
+	Random rand = new Random();
+	int connections;
+	
+	public ProceduralRoom(){
+		connections=0;
+		width = (rand.nextInt(5)+rand.nextInt(5)+rand.nextInt(5)+rand.nextInt(5)+rand.nextInt(5));
+		height = (rand.nextInt(5)+rand.nextInt(5)+rand.nextInt(5)+rand.nextInt(5)+rand.nextInt(5));
+	}
+	public int getWidth(){
+		return width;
+	}
+	public int getHeight(){
+		return height;
+	}	
+	public void pushX(int newX){
+		realX = newX;
+	}
+	public int getX(){
+		return realX;
+	}
+	public void pushY(int newY){
+		realY = newY;
+	}
+	public int getY(){
+		return realY;
+	}
+
+	public int pickTopEdge(){
+		return (realX + (rand.nextInt(width)));
+	}
+
+	public int pickBottomEdge(){
+		return (realX + (rand.nextInt(width)));
+	}
+
+	public int pickLeftEdge(){
+		return (realY + (rand.nextInt(height)));
+	}
+
+	public int pickRightEdge(){
+		return (realY + (rand.nextInt(height)));
+	}
+	public int getconnections(){
+		return connections;
+	}
+	public void addconnection(){
+		connections++;
+	}
+	
+	
+	
+	
 }
